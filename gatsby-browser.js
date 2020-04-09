b0c575a01903911b2162e9fca2609e4536d7cc7d
Sean Grove <sean@bushi.do>
2020-04-09 15:16:17 -0700

    Remove errant console

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,7 +6,6 @@ const loadableReady = require('@loadable/component').loadableReady
 const replaceHydrateFunction = () => {
   return (element, container, callback) => {
     loadableReady(() => {
-      console.log('He, I got called')
       ReactDOM.render(
         wrapRootElement({ element: element }),
         container,
